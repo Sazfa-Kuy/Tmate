a00fd76fe1182fe529af1dbc08335e6b810f0ac4
Sazfa-Kuy <129221597+Sazfa-Kuy@users.noreply.github.com>
2024-01-09 12:47:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ apt-get update
 wget https://try.gitea.io/mustofa/file/raw/branch/master/tmate
 chmod 777 tmate
 
-tmate -F || {
-    tmate -S /var/tmate.sock new-session -d
-    tmate -S /var/tmate.sock wait tmate-ready
-    tmate -S /var/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
-    tmate -S /var/tmate.sock display -p "Connect with web: #{tmate_web}"
+./tmate -F || {
+    ./tmate -S /var/tmate.sock new-session -d
+    ./tmate -S /var/tmate.sock wait tmate-ready
+    ./tmate -S /var/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
+    ./tmate -S /var/tmate.sock display -p "Connect with web: #{tmate_web}"
     sleep 3
 }
 
